17608a64fc20fbbc998f89f1bf6aca2531c217af
edoski <87473803+edoski@users.noreply.github.com>
2024-08-31 22:34:20 +0200

    fixing pt infinity8

--- firebase-init.js
+++ firebase-init.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-import * as dotenv from "dotenv";
+// import * as dotenv from "dotenv";
 
 // Load environment variables from .env file
-dotenv.config();
+// dotenv.config();
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID
+	// apiKey: process.env.FIREBASE_API_KEY,
+	// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: "DATABASE_URL_PLACEHOLDER",
+	// projectId: process.env.FIREBASE_PROJECT_ID,
+	// storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	// messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	// appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
