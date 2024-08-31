d1c9334a355c1e4f7d8cb2876b36e610e052b65a
edoski <87473803+edoski@users.noreply.github.com>
2024-08-31 23:36:34 +0200

    fixing pt infinity15

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-// require('dotenv').config()
-// const db = require('db')
 
 const firebaseConfig = {
 	// apiKey: process.env.FIREBASE_API_KEY,
 	// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	databaseURL: "https://web-chat-68f66-default-rtdb.europe-west1.firebasedatabase.app",
 	// projectId: process.env.FIREBASE_PROJECT_ID,
 	// storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	// messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
