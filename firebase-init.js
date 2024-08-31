94ba118aa3a547dd590308eabb54f7a846b3c81d
edoski <87473803+edoski@users.noreply.github.com>
2024-08-31 23:10:41 +0200

    fixing pt infinity13

--- firebase-init.js
+++ firebase-init.js
@@ -6,7 +6,7 @@ import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.13.0/fir
 const firebaseConfig = {
 	// apiKey: process.env.FIREBASE_API_KEY,
 	// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: "__FIREBASE_DATABASE_URL__",
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
 	// projectId: process.env.FIREBASE_PROJECT_ID,
 	// storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	// messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
