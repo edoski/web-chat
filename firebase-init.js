7a03fe18241cf839a80cde97766a631b73c0ae83
edoski <87473803+edoski@users.noreply.github.com>
2024-09-01 12:05:57 +0200

    fixing pt infinity20

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
-import dotenv from "dotenv";
+import dotenv from "https://cdn.skypack.dev/dotenv";
 export const config = dotenv.config();
 
 const firebaseConfig = {
